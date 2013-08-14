32dd361cb5e5e52ab1fe6f99bd05fc7317f31361
jay <jay@Superior.egr.msu.edu>
2013-08-14 16:47:34 -0400

    secret sharing lab done

--- secret_sharing_lab/secret_sharing_lab.py
+++ secret_sharing_lab/secret_sharing_lab.py
@@ -4,6 +4,7 @@
 import random
 from GF2 import one
 from vecutil import list2vec
+from vec import Vec
 
 
 
@@ -14,19 +15,23 @@ a0 = list2vec([one, one,   0, one,   0, one])
 b0 = list2vec([one, one,   0,   0,   0, one])
 
 def choose_secret_vector(s,t):
+    u = list2vec([randGF2() for i in range(len(a0.D))])
+    while a0*u != s or b0*u != t:
+        u = list2vec([randGF2() for i in range(len(a0.D))])
+    return u
 
 
 
 ## Problem 2
 # Give each vector as a Vec instance
-secret_a0 = ...
-secret_b0 = ...
-secret_a1 = ...
-secret_b1 = ...
-secret_a2 = ...
-secret_b2 = ...
-secret_a3 = ...
-secret_b3 = ...
-secret_a4 = ...
-secret_b4 = ...
+secret_a0 = Vec({0, 1, 2, 3, 4, 5},{0: one, 1: one, 2: 0, 3: one, 4: 0, 5: one})
+secret_b0 = Vec({0, 1, 2, 3, 4, 5},{0: one, 1: one, 2: 0, 3: 0, 4: 0, 5: one})
+secret_a1 = Vec({0, 1, 2, 3, 4, 5},{0: one, 1: one, 2: one, 3: 0, 4: 0, 5: one})
+secret_b1 = Vec({0, 1, 2, 3, 4, 5},{0: one, 1: 0, 2: 0, 3: one, 4: one, 5: one})
+secret_a2 = Vec({0, 1, 2, 3, 4, 5},{0: 0, 1: 0, 2: one, 3: one, 4: one, 5: one})
+secret_b2 = Vec({0, 1, 2, 3, 4, 5},{0: 0, 1: 0, 2: one, 3: 0, 4: 0, 5: one})
+secret_a3 = Vec({0, 1, 2, 3, 4, 5},{0: 0, 1: 0, 2: 0, 3: 0, 4: one, 5: one})
+secret_b3 = Vec({0, 1, 2, 3, 4, 5},{0: 0, 1: one, 2: one, 3: one, 4: one, 5: one})
+secret_a4 = Vec({0, 1, 2, 3, 4, 5},{0: one, 1: 0, 2: 0, 3: 0, 4: one, 5: 0})
+secret_b4 = Vec({0, 1, 2, 3, 4, 5},{0: one, 1: 0, 2: one, 3: one, 4: 0, 5: 0})
 
